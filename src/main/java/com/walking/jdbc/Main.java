bdde5da5107d2772b53d4d4b54306dec33f9ec2e
ааа <@d.d>
2024-02-17 23:37:54 +0400

    Lesson 129. The task

--- src/main/java/com/walking/jdbc/Main.java
+++ src/main/java/com/walking/jdbc/Main.java
@@ -7,6 +7,10 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
 
+/**
+ * Задача урока 129:
+ * <a href="https://github.com/KFalcon2022/lessons/blob/master/lessons/jdbc/129/Statement.%20DDL.%20ResultSet.md#%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0">ссылка</a>
+ */
 public class Main {
     private final static Logger log = LogManager.getLogger(Main.class);
 
